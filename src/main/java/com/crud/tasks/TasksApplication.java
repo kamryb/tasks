fc8fb4689ba8a1602f7ae81476776b4563469b07
kamryb <kamrybinski@gmail.com>
2020-11-21 12:37:44 +0100

    14.3v1

---
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -0,0 +1,31 @@
+package com.crud.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApplication {
+
+	public static void main(String[] args) {
+
+				SpringApplication.run(TasksApplication.class, args);
+
+
+	/* lombok test
+				TaskDto taskDto = new TaskDto(
+
+				(long)1,
+				"Test title",
+				"I want to be a ropper!"
+		);
+
+		Long id = taskDto.getId();
+		String title = taskDto.getTitle();
+		String content = taskDto.getContent();
+
+		System.out.println(id + " " + title + " " +content);
+*/
+
+	}
+
+}
