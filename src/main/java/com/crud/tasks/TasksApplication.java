2633043b464a0ee315cf3b38c923e33f6588b696
kamryb <kamrybinski@gmail.com>
2020-11-25 17:51:28 +0100

    15.2v1

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,16 +3,16 @@ package com.crud.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class TasksApplication {
-
 	public static void main(String[] args) {
-
+//		run(TasksApplication.class, args);
 				SpringApplication.run(TasksApplication.class, args);
 
-
-	/* lombok test
-				TaskDto taskDto = new TaskDto(
+	// lombok test
+/*				TaskDto taskDto = new TaskDto(
 
 				(long)1,
 				"Test title",
@@ -22,7 +22,6 @@ public class TasksApplication {
 		Long id = taskDto.getId();
 		String title = taskDto.getTitle();
 		String content = taskDto.getContent();
-
 		System.out.println(id + " " + title + " " +content);
 */
 
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,11 +1,14 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,12 +17,26 @@ import java.util.List;
 public class TaskController {
 
 
+ @Autowired
+    private DbService service;
+
+    @Autowired
+    private TaskMapper taskMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+       return taskMapper.mapToTaskDtoList(service.getAllTasks());
+      //  return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+   @RequestMapping(method = RequestMethod.GET, value = "getTaskID")
+   public TaskDto getTaskID() {
+      return (TaskDto) taskMapper.mapToTaskDto(service.getId(1L));
+      //  return new ArrayList<>();
+   }
+
+
+   @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
@@ -29,14 +46,14 @@ public class TaskController {
 
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "updateTask")
+    @RequestMapping(method = RequestMethod.PATCH, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "test content");
+
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "createTask")
-    public void createTask(TaskDto taskDto) {
-
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(@RequestBody TaskDto taskDto) {
 
 
     }
