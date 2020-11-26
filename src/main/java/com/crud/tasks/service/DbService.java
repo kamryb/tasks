a9657af01c380ab1b51a02ff2fb5e6d2d201a48b
kamryb <kamrybinski@gmail.com>
2020-11-26 19:53:26 +0100

    15.2v3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,17 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
     @Autowired
     private TaskRepository repository;
+    private Object TaskNotFoundException;
 
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
-    public Task getId(final Long id) {
-        return repository.findById(1L).orElse(null);}
+    public Optional<Task> getId(final Long id)  {
+        return repository.findById(1L);
+    }
 
  //   public Task getTaskById(final Long id) {
  //       return repository.findById(id).orElse(null);
